ab4601c1cc72b7348b5df78cc399e7c15ee449c0
SardineFish <Sardine@live.cn>
2017-12-29 16:45:26 +0800

    Simple LINQ & deferred execution

---
+++ DemoLINQ/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLINQ
+{
+    public class Animal
+    {
+
+        public Animal(string name, int age=0)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; } = "";
+        public int Age = 0;
+
+        public Animal Live()
+        {
+            Age++;
+            Console.WriteLine("{0} grown up.", Name);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return Name + ":" + Age.ToString();
+        }
+    }
+}
---
+++ DemoLINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateAndExecute();
+
+            Console.ReadKey();
+        }
+
+        public static void CreateAndExecute()
+        {
+            var animals = new Animal[] { new Animal("dog", 0), new Animal("cat", 1), new Animal("rabbit", 1) };
+            var query = from animal in animals where animal.Live().Age > 0 select animal;
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+            foreach (var animal in query)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
